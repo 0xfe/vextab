86438f6703550e8916fbadcb5ee1b801b204c1b0
Mohit Cheppudira <mohit@muthanna.com>
2012-12-08 13:59:07 -0500

    Fixed links in runtest.html.

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@
 # npm install jison
 # npm install coffee-script
 
+rm -rf build/
+mkdir build
 ./node_modules/.bin/jison src/vextab.jison -o build/vextab_parser.js
 ./node_modules/.bin/coffee --join build/vextab_coffee.js --compile src/*.coffee
-cp src/runtest.html build/
+cp -R src build/
+cp -R support build/
