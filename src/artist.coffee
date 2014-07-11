0xfe/vextab

commit a60809ee54ee07d4f7e90d6e116d04204a6202c1

Full caja support.

src/artist.coffee
@@ -913,19 +913,18 @@ class Vex.Flow.Artist
     tabstave_start_x = 40
 
     if opts.notation is "true"
-      note_stave = new Vex.Flow.Stave(@x, @last_y, @customizations.width - 20).addKeySignature(opts.key);
-      note_stave.addClef(opts.clef) if opts.clef isnt "none"
+      note_stave = new Vex.Flow.Stave(@x, @last_y, @customizations.width - 20).
+        addClef(opts.clef).addKeySignature(opts.key)
       note_stave.addTimeSignature(opts.time) if opts.time?
       @last_y += note_stave.getHeight() +
                  @options.note_stave_lower_spacing +
                  parseInt(@customizations["stave-distance"], 10)
       tabstave_start_x = note_stave.getNoteStartX()
-      @current_clef = if opts.clef is "none" then "treble" else opts.clef
+      @current_clef = opts.clef
 
     if opts.tablature is "true"
       tab_stave = new Vex.Flow.TabStave(@x, @last_y, @customizations.width - 20).
-        setNoteStartX(tabstave_start_x)
-      tab_stave.addTabGlyph() if opts.clef isnt "none"
+        addTabGlyph().setNoteStartX(tabstave_start_x)
       @last_y += tab_stave.getHeight() + @options.tab_stave_lower_spacing
 
     @closeBends()

doc/playground.html
@@ -10,7 +10,10 @@
   <script src="../support/vexflow-min.js"></script>
   <script src="../support/underscore-min.js"></script>
   <script src="../support/jquery.js"></script>
-  <script src="../support/tabdiv-min.js"></script>
+  <script>
+    Vex.Flow.Renderer.USE_CANVAS_PROXY = true;
+  </script>
+  <script src="../support/tabdiv-debug.js"></script>
 </head>
 
 <body>

commit a01157a6a9b31a5ad1cba50e24a7daa9e6a4d3dc

fix broken merge of #16
was broken in https://github.com/0xfe/vextab/commit/a60809ee54ee07d4f7e90d6e116d04204a6202c1

src/artist.coffee
@@ -956,21 +956,22 @@ class Vex.Flow.Artist
     start_x = @x + @customizations["connector-space"]
     tabstave_start_x = 40
 
-    if opts.notation is "true"
+   if opts.notation is "true"
       note_stave = new Vex.Flow.Stave(start_x, @last_y, @customizations.width - 20).
-        addClef(opts.clef).addKeySignature(opts.key)
+        addKeySignature(opts.key)
+      note_stave.addClef(opts.clef) if opts.clef isnt "none"
       note_stave.addTimeSignature(opts.time) if opts.time?
       @last_y += note_stave.getHeight() +
                  @options.note_stave_lower_spacing +
                  parseInt(@customizations["stave-distance"], 10)
       tabstave_start_x = note_stave.getNoteStartX()
-      @current_clef = opts.clef
+      @current_clef = if opts.clef is "none" then "treble" else opts.clef
 
     if opts.tablature is "true"
       tab_stave = new Vex.Flow.TabStave(start_x, @last_y, @customizations.width - 20)
         .setNumLines(opts.strings)
-        .addTabGlyph()
         .setNoteStartX(tabstave_start_x)
+      tab_stave.addTabGlyph() if opts.clef isnt "none"
       @last_y += tab_stave.getHeight() + @options.tab_stave_lower_spacing
 
     @closeBends()