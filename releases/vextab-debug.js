3d117f8bb5f26a343eaebef12060aced91e82457
Mohit Cheppudira <mohit@muthanna.com>
2014-11-25 14:53:05 -0500

    Committing release binaries for new version: 2.0.2

--- releases/vextab-debug.js
+++ releases/vextab-debug.js
@@ -1,5 +1,5 @@
 /**
- * VexTab 2.0.1 built on 2014-11-25.
+ * VexTab 2.0.2 built on 2014-11-25.
  * Copyright (c) 2010 Mohit Muthanna Cheppudira <mohit@muthanna.com>
  *
  * http://www.vexflow.com  http://github.com/0xfe/vextab
