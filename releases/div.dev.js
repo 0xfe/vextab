58554f136eff789cf6745d97466da7aff44b7293
Mo Chep <shhh@mo.town>
2026-01-17 20:36:46 -0500

    Committing release binaries for new version: 4.0.3

--- releases/div.dev.js
+++ releases/div.dev.js
@@ -24075,17 +24075,24 @@ class TextBuilder {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Artist: () => (/* reexport safe */ _artist__WEBPACK_IMPORTED_MODULE_1__["default"]),
+/* harmony export */   Div: () => (/* binding */ Div),
+/* harmony export */   Player: () => (/* reexport safe */ _player__WEBPACK_IMPORTED_MODULE_3__["default"]),
+/* harmony export */   Vex: () => (/* reexport safe */ _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"]),
+/* harmony export */   VexTab: () => (/* reexport safe */ _vextab__WEBPACK_IMPORTED_MODULE_2__["default"]),
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _vexflow__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./vexflow */ "./src/vexflow.ts");
 /* harmony import */ var _artist__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./artist */ "./src/artist.ts");
 /* harmony import */ var _vextab__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./vextab */ "./src/vextab.ts");
-/* harmony import */ var _vextab_css__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./vextab.css */ "./src/vextab.css");
+/* harmony import */ var _player__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./player */ "./src/player.ts");
+/* harmony import */ var _vextab_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./vextab.css */ "./src/vextab.css");
 /* provided dependency */ var $ = __webpack_require__(/*! zepto-webpack */ "./node_modules/zepto-webpack/zepto.js");
 /**
  * VexTab.Div renders VexTab code inside a DOM element, with optional
  * live-editing and error display support.
  */
+
 
 
 
@@ -24199,13 +24206,319 @@ class Div {
 window.VEXTAB_SEL_V3 = 'div.vextab-auto';
 function start(sel) {
     // eslint-disable-next-line
-    console.log('Running VexTab.Div:', "4.0.0-1-g6d79d73", "master", "6d79d73c74f58d0e55c75ab0b1004bb4d9dcfccb");
+    console.log('Running VexTab.Div:', "4.0.2-1-g0646fe9", "master", "0646fe9fb14e299c4b58fb1a126610f1d6758c0b");
     $(sel || window.VEXTAB_SEL_V3).forEach((s) => new Div(s));
 }
 $(() => { if (window.VEXTAB_SEL_V3) {
     start();
 } });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Div);
+
+
+
+/***/ },
+
+/***/ "./src/player.ts"
+/*!***********************!*\
+  !*** ./src/player.ts ***!
+  \***********************/
+(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _vexflow__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./vexflow */ "./src/vexflow.ts");
+/* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils */ "./src/utils.ts");
+/* provided dependency */ var $ = __webpack_require__(/*! zepto-webpack */ "./node_modules/zepto-webpack/zepto.js");
+
+
+/**
+ * VexTab Player
+ *
+ * This helper renders a transport overlay and uses MIDI.js to play back the
+ * rendered notes. It attaches itself to Vex.Flow.Player for compatibility
+ * with legacy VexTab usage.
+ */
+class Player {
+    constructor(artist, options) {
+        this.interval_id = null;
+        this.paper = null;
+        this.marker = null;
+        this.loading_message = null;
+        this.play_button = null;
+        this.stop_button = null;
+        this.tick_notes = {};
+        this.all_ticks = [];
+        this.total_ticks = null;
+        this.tpm = 0;
+        this.refresh_rate = 25;
+        this.ticks_per_refresh = 0;
+        this.current_ticks = 0;
+        this.next_event_tick = 0;
+        this.next_index = 0;
+        this.done = false;
+        this.loading = false;
+        this.scale = 1;
+        // Static constants pulled from VexFlow for timing.
+        this.Fraction = _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.Fraction;
+        this.RESOLUTION = _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.RESOLUTION;
+        this.noteValues = _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.Music.noteValues;
+        this.INSTRUMENTS = {
+            acoustic_grand_piano: 0,
+            acoustic_guitar_nylon: 24,
+            acoustic_guitar_steel: 25,
+            electric_guitar_jazz: 26,
+            distortion_guitar: 30,
+            electric_bass_finger: 33,
+            electric_bass_pick: 34,
+            trumpet: 56,
+            brass_section: 61,
+            soprano_sax: 64,
+            alto_sax: 65,
+            tenor_sax: 66,
+            baritone_sax: 67,
+            flute: 73,
+            synth_drum: 118,
+        };
+        this.artist = artist;
+        this.log('Initializing player: ', options);
+        this.options = {
+            instrument: 'acoustic_grand_piano',
+            tempo: 120,
+            show_controls: true,
+            soundfont_url: '/soundfont/',
+            overlay_class: 'vextab-player',
+        };
+        if (options) {
+            _utils__WEBPACK_IMPORTED_MODULE_1__.extend(this.options, options);
+        }
+        this.log(`Using soundfonts in: ${this.options.soundfont_url}`);
+        this.reset();
+    }
+    log(...args) {
+        if (Player.DEBUG && console) {
+            console.log('(Vex.Flow.Player)', ...args);
+        }
+    }
+    setArtist(artist) {
+        this.artist = artist;
+        this.reset();
+    }
+    setTempo(tempo) {
+        this.log('New tempo: ', tempo);
+        this.options.tempo = tempo;
+        this.reset();
+    }
+    setInstrument(instrument) {
+        this.log('New instrument: ', instrument);
+        if (!Object.keys(this.INSTRUMENTS).includes(instrument)) {
+            throw new _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].RERR('PlayerError', `Invalid instrument: ${instrument}`);
+        }
+        this.options.instrument = instrument;
+        this.reset();
+    }
+    reset() {
+        this.artist.attachPlayer(this);
+        this.tick_notes = {};
+        this.all_ticks = [];
+        this.tpm = this.options.tempo * (this.RESOLUTION / 4);
+        this.refresh_rate = 25; // ms: 50 = 20hz
+        this.ticks_per_refresh = this.tpm / (60 * (1000 / this.refresh_rate));
+        this.total_ticks = 0;
+        if (this.marker) {
+            this.marker.remove();
+            this.marker = null;
+        }
+        this.stop();
+    }
+    getOverlay(context, scale, overlay_class) {
+        const canvas = context.canvas;
+        const height = canvas.height;
+        const width = canvas.width;
+        const overlay = $('<canvas>');
+        overlay.css('position', 'absolute');
+        overlay.css('left', 0);
+        overlay.css('top', 0);
+        overlay.addClass(overlay_class);
+        $(canvas).after(overlay);
+        const ctx = _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.Renderer.getCanvasContext(overlay.get(0), width, height);
+        ctx.scale(scale, scale);
+        const ps = new paper.PaperScope();
+        ps.setup(overlay.get(0));
+        return { paper: ps, canvas: overlay.get(0) };
+    }
+    removeControls() {
+        if (this.play_button)
+            this.play_button.remove();
+        if (this.stop_button)
+            this.stop_button.remove();
+        if (this.paper)
+            this.paper.view.draw();
+    }
+    render() {
+        this.reset();
+        const data = this.artist.getPlayerData();
+        this.scale = data.scale;
+        if (!this.paper) {
+            const overlay = this.getOverlay(data.context, data.scale, this.options.overlay_class);
+            this.paper = overlay.paper;
+        }
+        this.marker = new this.paper.Path.Rectangle(0, 0, 13, 85);
+        this.loading_message = new this.paper.PointText(35, 12);
+        if (this.options.show_controls) {
+            this.play_button = new this.paper.Path.RegularPolygon(new this.paper.Point(25, 10), 3, 7, 7);
+            this.play_button.fillColor = '#396';
+            this.play_button.opacity = 0.8;
+            this.play_button.rotate(90);
+            this.play_button.onMouseUp = () => {
+                this.play();
+            };
+            this.stop_button = new this.paper.Path.Rectangle(3, 3, 10, 10);
+            this.stop_button.fillColor = '#396';
+            this.stop_button.opacity = 0.8;
+            this.stop_button.onMouseUp = () => {
+                this.stop();
+            };
+        }
+        this.paper.view.draw();
+        const staves = data.voices;
+        let total_ticks = new this.Fraction(0, 1);
+        staves.forEach((voice_group) => {
+            let max_voice_tick = new this.Fraction(0, 1);
+            voice_group.forEach((voice) => {
+                const total_voice_ticks = new this.Fraction(0, 1);
+                voice.getTickables().forEach((note) => {
+                    if (!note.shouldIgnoreTicks()) {
+                        const abs_tick = total_ticks.clone();
+                        abs_tick.add(total_voice_ticks);
+                        abs_tick.simplify();
+                        const key = abs_tick.toString();
+                        if (_utils__WEBPACK_IMPORTED_MODULE_1__.has(this.tick_notes, key)) {
+                            this.tick_notes[key].notes.push(note);
+                        }
+                        else {
+                            this.tick_notes[key] = {
+                                tick: abs_tick,
+                                value: abs_tick.value(),
+                                notes: [note],
+                            };
+                        }
+                        total_voice_ticks.add(note.getTicks());
+                    }
+                });
+                if (total_voice_ticks.value() > max_voice_tick.value()) {
+                    max_voice_tick.copy(total_voice_ticks);
+                }
+            });
+            total_ticks.add(max_voice_tick);
+        });
+        this.all_ticks = _utils__WEBPACK_IMPORTED_MODULE_1__.sortBy(_utils__WEBPACK_IMPORTED_MODULE_1__.values(this.tick_notes), (tick) => tick.value);
+        this.total_ticks = _utils__WEBPACK_IMPORTED_MODULE_1__.last(this.all_ticks);
+        this.log(this.all_ticks);
+    }
+    updateMarker(x, y) {
+        this.marker.fillColor = '#369';
+        this.marker.opacity = 0.2;
+        this.marker.setPosition(new this.paper.Point(x * this.scale, y * this.scale));
+        this.paper.view.draw();
+    }
+    playNote(notes) {
+        this.log(`(${this.current_ticks}) playNote: `, notes);
+        notes.forEach((note) => {
+            const x = note.getAbsoluteX() + 4;
+            const y = note.getStave().getYForLine(2);
+            if (this.paper)
+                this.updateMarker(x, y);
+            if (note.isRest())
+                return;
+            const keys = note.getPlayNote();
+            const duration = note.getTicks().value() / (this.tpm / 60);
+            keys.forEach((key) => {
+                const pieces = key.split('/');
+                const noteName = pieces[0].trim().toLowerCase();
+                const octave = pieces[1];
+                const note_value = this.noteValues[noteName];
+                if (!note_value)
+                    return;
+                const midi_note = (24 + (parseInt(octave, 10) * 12)) + note_value.int_val;
+                MIDI.noteOn(0, midi_note, 127, 0);
+                MIDI.noteOff(0, midi_note, duration);
+            });
+        });
+    }
+    refresh() {
+        if (this.done) {
+            this.stop();
+            return;
+        }
+        this.current_ticks += this.ticks_per_refresh;
+        if (this.current_ticks >= this.next_event_tick && this.all_ticks.length > 0) {
+            this.playNote(this.all_ticks[this.next_index].notes);
+            this.next_index += 1;
+            if (this.next_index >= this.all_ticks.length) {
+                this.done = true;
+            }
+            else {
+                this.next_event_tick = this.all_ticks[this.next_index].tick.value();
+            }
+        }
+    }
+    stop() {
+        this.log('Stop');
+        if (this.interval_id)
+            window.clearInterval(this.interval_id);
+        if (this.play_button)
+            this.play_button.fillColor = '#396';
+        if (this.paper)
+            this.paper.view.draw();
+        this.interval_id = null;
+        this.current_ticks = 0;
+        this.next_event_tick = 0;
+        this.next_index = 0;
+        this.done = false;
+    }
+    start() {
+        this.stop();
+        this.log('Start');
+        if (this.play_button)
+            this.play_button.fillColor = '#a36';
+        MIDI.programChange(0, this.INSTRUMENTS[this.options.instrument]);
+        this.render();
+        this.interval_id = window.setInterval(() => this.refresh(), this.refresh_rate);
+    }
+    play() {
+        this.log('Play: ', this.refresh_rate, this.ticks_per_refresh);
+        if (Player.INSTRUMENTS_LOADED[this.options.instrument] && !this.loading) {
+            this.start();
+        }
+        else {
+            this.log('Loading instruments...');
+            this.loading_message.content = 'Loading instruments...';
+            this.loading_message.fillColor = 'green';
+            this.loading = true;
+            this.paper.view.draw();
+            MIDI.loadPlugin({
+                soundfontUrl: this.options.soundfont_url,
+                instruments: [this.options.instrument],
+                callback: () => {
+                    Player.INSTRUMENTS_LOADED[this.options.instrument] = true;
+                    this.loading = false;
+                    this.loading_message.content = '';
+                    this.start();
+                },
+            });
+        }
+    }
+}
+Player.DEBUG = false;
+Player.INSTRUMENTS_LOADED = {};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Player);
+// Preserve legacy access via Vex.Flow.Player.
+if (!_vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.Player) {
+    _vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow.Player = Player;
+}
 
 
 /***/ },
@@ -24364,6 +24677,10 @@ if (!vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].Flow) {
 // Legacy error constructor used by older VexFlow APIs.
 if (!vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].RERR && vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].RuntimeError) {
     vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].RERR = vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].RuntimeError;
+}
+// Legacy benchmark helper used by older VexFlow APIs.
+if (!vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].BM) {
+    vexflow__WEBPACK_IMPORTED_MODULE_0__["default"].BM = (_label, fn) => fn();
 }
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (vexflow__WEBPACK_IMPORTED_MODULE_0__["default"]);
 
